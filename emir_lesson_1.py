4270ddd15923ccf6dbbe093f3ff59973cc54c12c
cem emir <cememir2017@gmail.com>
2024-05-02 21:23:03 +0300

    listeleri birleştirmek

---
+++ emir_lesson_1.py
@@ -0,0 +1,10 @@
+vatan = ['ömer', 'tugkan', 'emir']
+ugur = ['barış', 'ahmet', 'emir2']
+yfo = ['tuna', 'aytug', 'eymen']
+
+butun_arkadaslarim = vatan + ugur + yfo
+# butun_arkadaslarim = []
+# butun_arkadaslarim.extend(vatan)
+# butun_arkadaslarim.extend(ugur)
+# butun_arkadaslarim.extend(yfo)
+print(butun_arkadaslarim)
