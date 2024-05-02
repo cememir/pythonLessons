69f1495dad0b917c489c3fb144b476bc07933c03
cem emir <cememir2017@gmail.com>
2024-05-02 21:34:37 +0300

    string methodları

---
+++ emir_lesson_2.py
@@ -0,0 +1,36 @@
+string_var = "hello"
+string_var2 = string_var * 3
+print(string_var2)
+
+#############################################################################
+#############################################################################
+
+bir = 10
+iki = 2
+
+print(bir / iki)
+
+#############################################################################
+#############################################################################
+
+string_var = "hello"
+print(string_var[-1])
+
+#############################################################################
+#############################################################################
+string_var = "hello, world"
+print(string_var.find("o"))  # 4
+print(string_var.count("l"))  # 3
+print(string_var.upper())  # "HELLO, WORLD"
+print(string_var.lower())  # "hello, world"
+print(string_var.title())  # "Hello, World"
+print(string_var.capitalize())  # "Hello, world"
+print(string_var.strip())  # "hello, world"
+print(string_var.split(", "))  # ["hello", "world"]
+
+#############################################################################
+#############################################################################
+
+merhaba= """muslu'nun selam"ı var"""
+
+print(merhaba)
